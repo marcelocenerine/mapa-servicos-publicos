5cd2b9b9fbc33de1dea8b784cf08726521cfafa2
marcelocenerine <marcelocenerine@gmail.com>
2013-10-04 00:56:40 -0300

    Criação de atributo 'fax'.

--- src/main/java/br/com/servicospublicos/model/Contato.java
+++ src/main/java/br/com/servicospublicos/model/Contato.java
@@ -10,6 +10,7 @@ public class Contato {
 
 	private String email;
 	private List<String> telefones;
+	private List<String> fax;
 
 	public String getEmail() {
 		return email;
@@ -25,5 +26,13 @@ public class Contato {
 
 	public void setTelefones(List<String> telefones) {
 		this.telefones = telefones;
+	}
+
+	public List<String> getFax() {
+		return fax;
+	}
+
+	public void setFax(List<String> fax) {
+		this.fax = fax;
 	}
 }
