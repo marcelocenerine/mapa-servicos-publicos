0a850ff39dd9f45005d7025a142d9d8ed53532d5
marcelocenerine <marcelocenerine@gmail.com>
2013-10-08 17:07:42 -0300

    - Organizada no javascript - Esconder street view antes de realizar cada pesquisa;

--- src/main/webapp/static/js/mapaservpub.js
+++ src/main/webapp/static/js/mapaservpub.js
@@ -1,63 +1,97 @@
-	var map;
-	var marker;
-	var geocoder;
-	var markers = [];
-	var infowindow = new google.maps.InfoWindow();
-	var servicos = 'CORREIOS, CARTORIO, ENS_BASICO, ENS_SUPERIOR, RFB, ASS_SOCIAL, INSS, COM_TERAP, SINE, UBS';
-			  
-	$(document).ready(function () {
+var map;
+var geocoder;
+var markers = [];
+var infowindow = new google.maps.InfoWindow();
+var defaultZoom = 4;
+var placeZoom = 15;
+var defaultLat = -14.235004;
+var defaultlng = -51.92528;
 	
-	getLocation();
 	
-	//Busca geolocalizacao do usuario
-	function getLocation() {
-		var latdefault = -13.0000000;
-		var longdefault = -47.000000;
-		initialize(latdefault, longdefault, 4, 2, false);
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(
-				function (position) {
-					initialize(position.coords.latitude, position.coords.longitude, 14, 1, true);
-				},
-				function (erro) {
-					initialize(latdefault, longdefault, 4, 2, false);
-				}
-			);
-		}
+function showWhereIam() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(
+			function (position) {
+				defineSearchPlace(position.coords.latitude, position.coords.longitude);
+				loadPoints(position.coords.latitude, position.coords.longitude);
+			}
+		);
 	}
-	
-	//Inicializacao do site
-	function initialize(latitude, longitude, zoom, enderecodesc, pontosmapa) {
-		var latlng = new google.maps.LatLng(latitude, longitude);
-		var options = {
-			zoom: zoom,
-			center: latlng,
-			panControl: true,
-			streetViewControl: true,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
-		map = new google.maps.Map(document.getElementById('mapa'), options);
-		
-		geocoder = new google.maps.Geocoder();
-		marker = new google.maps.Marker({
-			map: map,
-			draggable: false,
-		});
-		marker.setPosition(latlng);
+}
 
-		geocoder.geocode({'latLng': latlng}, function(results, status) {
-		  if (status == google.maps.GeocoderStatus.OK) {
-			if (results[enderecodesc]) {
-					$("#txtEndereco").val(results[enderecodesc].formatted_address);
+function defineSearchPlace(lat, lng) {
+	var location = new google.maps.LatLng(lat, lng);
+	var marker = new google.maps.Marker({
+		map: map,
+		draggable: false,
+	});
+	marker.setPosition(location);
+	map.setCenter(location);
+	map.setZoom(placeZoom);
+}
+
+function loadPoints(latitude, longitude) {
+	clearMarkers();
+	hideStreetView();
+	// APAGAR LINHA ABAIXO QUANDO A CONSULTA POR CATEGORIAS ESTIVER PRONTA
+	var servicos = 'CORREIOS, CARTORIO, ENS_BASICO, ENS_SUPERIOR, RFB, ASS_SOCIAL, INSS, COM_TERAP, SINE, UBS';
+	
+	$.getJSON('rest/api/servicos/lng/' + longitude + '/lat/' + latitude + '/categorias/' + servicos, function(pontos) {
+		$.each(pontos, function(index, ponto) {
+			addPoint(ponto);
+		});
+	});	
+}
+
+function addPoint(ponto) {
+	var marker = new google.maps.Marker({
+		position: new google.maps.LatLng(ponto.localizacao.coordenadas.latitude, ponto.localizacao.coordenadas.longitude),
+		title: ponto.categoria.descricao,
+		map: map,
+		icon: 'img/' + ponto.categoria.tipo + '.png'
+	});
+	trackMarker(marker);
+
+	google.maps.event.addListener(marker, 'click', (function(marker, i) {
+		return function() {
+			var msgbalao = "<p><strong>" + ponto.categoria.descricao + "</strong></p>";
+			if (ponto.nome) {
+				msgbalao += "<p>" + ponto.nome + "</p>";
+			}
+			if (ponto.atendimento) {
+				msgbalao += "<p>Atendimento: " + ponto.atendimento + "</p>";
+			}
+			if (ponto.contato) {
+				if (ponto.contato.telefones) {
+					msgbalao += "<p>Telefones: " + ponto.contato.telefones.join(', ') + "</p>";
+				}
+				if (ponto.contato.email) {
+					msgbalao += "<p>Email: " + ponto.contato.email; + "</p>";
 				}
 			}
-		});		
-		
-		if (pontosmapa == true) {
-			setPontosMapa(latitude, longitude, servicos);
+			infowindow.setContent(msgbalao);
+			infowindow.open(map, marker);
+		};
+	})(marker));
+}
+
+function trackMarker(marker) {
+	markers.push(marker);
+}
+
+function clearMarkers() {
+	$(markers).each(function(i, e) { 
+		e.setMap(null);
 		}
-	}
-	
+	);
+	markers = [];
+}
+
+function hideStreetView() {
+	map.getStreetView().setVisible(false);
+}
+
+function bindComponentEvents() {
 	//Sugere enderecos  eexibe no mapa o local clicado
 	$("#txtEndereco").autocomplete({
 		source: function (request, response) {
@@ -73,63 +107,23 @@
 			});
 		},
 		select: function (event, ui) {
-			var location = new google.maps.LatLng(ui.item.latitude, ui.item.longitude);
-			marker.setPosition(location);
-			map.setCenter(location);
-			map.setZoom(14);
-			setPontosMapa(ui.item.latitude, ui.item.longitude, servicos);
+			defineSearchPlace(ui.item.latitude, ui.item.longitude);
+			loadPoints(ui.item.latitude, ui.item.longitude);
 		}
-	});
-	
-	
-	//Exibe no mapa servicos publicos desejados
-	function setPontosMapa(latitude, longitude, servicos) {
-		limparMarcadores();
-		$.getJSON('rest/api/servicos/lng/'+longitude+'/lat/'+latitude+'/categorias/'+servicos, function(pontos) {
-			$.each(pontos, function(index, ponto) {
-					var marker = new google.maps.Marker({
-						position: new google.maps.LatLng(ponto.localizacao.coordenadas.latitude, ponto.localizacao.coordenadas.longitude),
-						title: ponto.categoria.descricao,
-						map: map,
-						icon: 'img/' + ponto.categoria.tipo + '.png'
-					});
-					rastrearMarcador(marker);
+	});	
+}
 
-					google.maps.event.addListener(marker, 'click', (function(marker, i) {
-						return function() {
-							var msgbalao = "<p><strong>" + ponto.categoria.descricao + "</strong></p>";
-							if (ponto.nome) {
-								msgbalao += "<p>" + ponto.nome + "</p>";
-							}
-							if (ponto.atendimento) {
-								msgbalao += "<p>Atendimento: " + ponto.atendimento + "</p>";
-							}
-							if (ponto.contato) {
-								if (ponto.contato.telefones) {
-									msgbalao += "<p>Telefones: " + ponto.contato.telefones.join(', ') + "</p>";
-								}
-								if (ponto.contato.email) {
-									msgbalao += "<p>Email: " + ponto.contato.email; + "</p>";
-								}
-							}
-							infowindow.setContent(msgbalao);
-							infowindow.open(map, marker);
-						};
-					})(marker));
-			});
-		});	
-	}
+$(document).ready(function () {
+	var options = {
+		zoom: defaultZoom,
+		center: new google.maps.LatLng(defaultLat, defaultlng),
+		panControl: true,
+		streetViewControl: true,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	map = new google.maps.Map(document.getElementById('mapa'), options);
+	geocoder = new google.maps.Geocoder();
 	
-	function rastrearMarcador(marker) {
-		markers.push(marker);
-	}
-	
-	function limparMarcadores() {
-		$(markers).each(function(i, e) { 
-			e.setMap(null);
-			}
-		);
-		markers = [];
-	}
-	
+	bindComponentEvents();
+	showWhereIam();
 });
