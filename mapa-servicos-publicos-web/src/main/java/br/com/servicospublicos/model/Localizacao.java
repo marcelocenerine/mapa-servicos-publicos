4a8ced41f8117718449360b63d0590550754cd81
Marcelo Cenerino <marcelocenerine@gmail.com>
2015-05-09 16:54:55 +0100

    moves files to subfolder

--- mapa-servicos-publicos-web/src/main/java/br/com/servicospublicos/model/Localizacao.java
+++ mapa-servicos-publicos-web/src/main/java/br/com/servicospublicos/model/Localizacao.java
@@ -3,9 +3,9 @@ package br.com.servicospublicos.model;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
 
-@JsonSerialize(include=Inclusion.NON_NULL)
+@JsonSerialize(include = Inclusion.NON_NULL)
 public class Localizacao {
-	
+
 	private String endereco;
 	private String complemento;
 	private String bairro;
@@ -13,15 +13,15 @@ public class Localizacao {
 	private UF uf;
 	private String cep;
 	private Coordenadas coordenadas;
-	
+
 	public String getEndereco() {
 		return endereco;
 	}
-	
+
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
 	}
-	
+
 	public String getComplemento() {
 		return complemento;
 	}
@@ -33,39 +33,39 @@ public class Localizacao {
 	public String getBairro() {
 		return bairro;
 	}
-	
+
 	public void setBairro(String bairro) {
 		this.bairro = bairro;
 	}
-	
+
 	public String getCidade() {
 		return cidade;
 	}
-	
+
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
 	}
-	
+
 	public UF getUf() {
 		return uf;
 	}
-	
+
 	public void setUf(UF uf) {
 		this.uf = uf;
 	}
-	
+
 	public String getCep() {
 		return cep;
 	}
-	
+
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
-	
+
 	public Coordenadas getCoordenadas() {
 		return coordenadas;
 	}
-	
+
 	public void setCoordenadas(Coordenadas coordenadas) {
 		this.coordenadas = coordenadas;
 	}
